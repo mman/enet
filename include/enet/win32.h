cf46ab34fe70681752ac7cdfa54352b07f35adca
proller <proller@github.com>
2015-04-06 03:20:12 +0300

    win32: fix include order

--- include/enet/win32.h
+++ include/enet/win32.h
@@ -22,11 +22,10 @@
 	#define _WIN32_WINNT 0x0501
 #endif
 #define NOMINMAX
-#include <windows.h>
-#include <ws2tcpip.h>
-
 #include <stdlib.h>
 #include <winsock2.h>
+#include <windows.h>
+#include <ws2tcpip.h>
 
 typedef SOCKET ENetSocket;
 
